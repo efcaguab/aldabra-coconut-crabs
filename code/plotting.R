1f969b597dd333e01f78e4654a9fd5c1da071956
Fernando Cagua <efcaguab@gmail.com>
2019-03-26 12:14:14 +1300

    Some plotting aid functions

--- code/plotting.R
+++ code/plotting.R
@@ -17,10 +17,12 @@ extract_fit <- function(y, rename = NULL){
   } else {
     d <- dplyr::data_frame(x = y$x)
   }
+  fit <- y$fit[, 1]
+  se <- y$se
   d %<>%
-    dplyr::mutate(fit = y$fit[, 1],
-                  fitmax = fit + y$se,
-                  fitmin = fit - y$se)
+    dplyr::mutate(fit = fit,
+                  fitmax = fit + se,
+                  fitmin = fit - se)
   
   if(!is.null(rename)) {
     names(d)[names(d) == "fit"] <- paste(y$xlab, "fit", sep = "_")
@@ -33,16 +35,21 @@ extract_fit <- function(y, rename = NULL){
 pub_theme <- function(){
   theme_bw() +
     theme(strip.background = element_blank(),
+          strip.text = element_text(size = 8),
           plot.margin = grid::unit(c(0.4,0.4, 0.4, 0.4), "lines"),
           text = element_text(size = 8),
           legend.title = element_text(size = 14),
-          plot.title = element_text(face = "bold", size = 8, margin = margin()),
+          plot.title = element_text(face = "bold", size = 8, margin = margin(b = 1)),
           plot.subtitle = element_text(size = 8),
           panel.grid = element_blank(),
           legend.position = c(0,0),
           legend.direction = "horizontal",
           legend.key.size = grid::unit(0.7, "lines"),
-          legend.margin = grid::unit(0, "lines"),
+          legend.margin = margin(),
           legend.justification = c(0,0),
           legend.background = element_rect(fill = alpha('white', 0.5)))
+}
+
+colour_scale <- function(){
+  scales::viridis_pal(begin = 0.2, end = 0.7, option = "D")(2) 
 }
---
+++ code/stat_subset.R
@@ -0,0 +1,64 @@
+#' Subset values
+#'
+#' Removes values where `subset` evaluates to `FALSE`. Useful for showing only
+#' statistical significant values, or an interesting subset of the data without
+#' manually subsetting the data.
+#'
+#' @inheritParams ggplot2::stat_identity
+#'
+#' @section Aesthetics:
+#' `stat_subset` understands the following aesthetics (required aesthetics are in bold)
+#'
+#' \itemize{
+#' \item **x**
+#' \item **y**
+#' \item **subset**
+#' \item `width`
+#' \item `height`
+#' }
+#'
+#' @examples
+#'
+#' library(ggplot2)
+#' ggplot(data.table::melt(volcano), aes(Var1, Var2)) +
+#'     geom_contour(aes(z = value)) +
+#'     stat_subset(aes(subset = value >= 150 & value <= 160),
+#'                 shape = 3, color = "red")
+#'
+#' @seealso [stat_na] for a more specialized stat for filtering `NA` values.
+#' @export
+#' @family ggplot2 helpers
+stat_subset <- function(mapping = NULL, data = NULL,
+                    geom = "point", position = "identity",
+                    ...,
+                    show.legend = NA,
+                    inherit.aes = TRUE) {
+    layer(
+        data = data,
+        mapping = mapping,
+        stat = StatSubset,
+        geom = geom,
+        position = position,
+        show.legend = show.legend,
+        inherit.aes = inherit.aes,
+        params = list(
+            ...
+        )
+    )
+}
+
+#' @import ggplot2
+#' @import scales
+#' @rdname stat_subset
+#' @usage NULL
+#' @format NULL
+#' @export
+StatSubset <- ggplot2::ggproto("StatSubset", ggplot2::Stat,
+  required_aes = c("x", "y", "subset"),
+  compute_group = function(data, scales, width = NULL, height = NULL) {
+      data$width <- data$width %||% width %||% resolution(data$x, FALSE)
+      data$height <- data$height %||% height %||% resolution(data$y, FALSE)
+      data <- subset(data, subset == TRUE)
+      data
+  }
+)
